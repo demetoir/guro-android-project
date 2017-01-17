a54e0d29642446847fed179b03dd69f529c56591
Yujun-desktop <wnsqlehlswk@naver.com>
2017-01-18 01:41:34 +0900

    숙제 6 하는중

--- home_work/Quiz_app/app/src/main/java/me/demetoir/quiz_app/MainActivityFragment.java
+++ home_work/Quiz_app/app/src/main/java/me/demetoir/quiz_app/MainActivityFragment.java
@@ -20,23 +20,52 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        Button bnt_O = (Button) rootView.findViewById(R.id.fragment_quiz_answer_O);
-        Button bnt_X = (Button) rootView.findViewById(R.id.fragment_quiz_answer_X);
-//string 제대로 다 R에서 가져와야함
+        Button bntO = (Button) rootView.findViewById(R.id.fragment_quiz_answer_O);
+        Button bntX = (Button) rootView.findViewById(R.id.fragment_quiz_answer_X);
+        Button bntPrev = (Button) rootView.findViewById(R.id.fragment_button_prev_quiz);
+        Button bntNext = (Button) rootView.findViewById(R.id.fragment_button_next_quiz);
 
-        bnt_O.setOnClickListener(new View.OnClickListener() {
+
+        bntO.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(),R.string., Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(),R.string.button_correct, Toast.LENGTH_SHORT).show();
             }
         });
-        bnt_X.setOnClickListener(new View.OnClickListener() {
+        bntX.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "wrong", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(),R.string.button_wrong, Toast.LENGTH_SHORT).show();
             }
         });
 
+        //// 버튼 감추기
+        //bnt_O.setVisibility(View.GONE);
+        //1번째일때는 나타나지 않아야한다
+        bntPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+//        10번째에서는 누르면 다음페이지로 넘어가야함
+        bntNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return rootView;
+    }
+
+    //string 리소스를 접근,관리해야한다??
+    //mvc모델?
+    //http://bsnippet.tistory.com/13
+
+    public class Quiz{
+        Quiz(){
+
+        }
+
     }
 }
